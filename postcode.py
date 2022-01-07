41a5d1b06e5484482c8b83f5e8708054b19f8cb6
boson.lai <97293165+boson0725@users.noreply.github.com>
2022-01-07 22:29:51 +0800

    Add files via upload

---
+++ postcode.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 18:01:27 2022
+
+@author: user
+"""
+
+
+
+#input
+print("請輸入郵遞區號")
+post_code = input()
+
+if post_code == "802":
+   address="苓雅區"
+   
+elif post_code == "801":
+     address="前金區"
+     
+elif post_code == "803":
+     address="鹽埕區"
+     
+elif post_code == "800":
+     address="新興區"
+elif post_code == "804":
+     address="鼓山區"
+elif post_code == "805":
+     address="旗津區"
+elif post_code == "806":
+     address="前鎮區"
+elif post_code == "807":
+     address="三民區"
+elif post_code == "811":
+     address="楠梓區"
+elif post_code == "812":
+     address="小港區"
+elif post_code == "813":
+     address="左營區"
+elif post_code == "814":
+     address="仁武區"
+elif post_code == "815":
+     adress="大社區"
+elif post_code == "820":
+     address="岡山區"
+elif post_code == "821":
+     address="路竹區"
+elif post_code == "822":
+     address="阿蓮區"
+elif post_code == "823":
+     address="田寮區"
+elif post_code == "824":
+     address="燕巢區"
+elif post_code == "825":
+     address="橋頭區"
+elif post_code == "826":
+     address="梓官區"
+elif post_code == "827":
+     address="彌陀區"
+elif post_code == "828":
+     address="永安區"
+elif post_code == "829":
+     address="湖內區"
+elif post_code == "830":
+     address="鳳山區"
+elif post_code == "831":
+     address="大寮區"     
+elif post_code == "832":
+     address="林園區"    
+elif post_code == "833":
+     address="鳥松區"
+elif post_code == "840":
+     address="大樹區"
+elif post_code == "842":
+     address="旗山區"
+elif post_code == "843":
+     address="美濃區"
+elif post_code == "844":
+     address="六龜區"
+elif post_code == "845":
+     address="內門區"
+elif post_code == "846":
+     address="杉林區"
+elif post_code == "847":
+     address="甲仙區"
+elif post_code == "848":
+     address="桃源區"
+elif post_code == "849":
+     address="那瑪夏區"
+elif post_code == "851":
+     address="茂林區"
+elif post_code == "852":
+     address="茄萣區"     
+     
+else : 
+    address="查無此區"
+    
+print("郵遞番號"+post_code+"為"+address)
+
+input()
